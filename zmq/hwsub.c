630b0cc4d1e826e694cab29ad14144a4c8e5990b
Thomas Lindner <lindner@triumf.ca>
2019-03-11 10:46:17 -0700

    Changes to ZMQ stuff.

--- zmq/hwsub.c
+++ zmq/hwsub.c
@@ -1,5 +1,3 @@
-//  Hello World server
-
 #include <zmq.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -13,19 +11,21 @@ int main (void)
     void *context = zmq_ctx_new ();
     void *subscriber = zmq_socket (context, ZMQ_SUB);
     int rc = zmq_bind (subscriber, "tcp://*:5555");
-    // With message
-    //    zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, "Z", 1);
+    printf ("*context: %p *subscriber: %p rc:%d \n", context, subscriber, rc);
+    printf("%s\n", strerror(errno));
+    assert (rc==0);
+
     // Without message
     zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, "", 0);
-    printf ("*context: %p *subscriber: %p rc:%d \n", context, subscriber, rc);
-    //    printf("%s\n", strerror(errno));
 
     while (1) {
       uint32_t rcvbuf [100];
       int stat = zmq_recv (subscriber, rcvbuf, sizeof(rcvbuf), 0);
-      //      int stat = zmq_recv (subscriber, rcvbuf, sizeof(rcvbuf), ZMQ_DONTWAIT);
-      //      printf("%s -- ", strerror(errno));
       printf ("stat: %d - rcvbuf[0]: %d\n", stat, rcvbuf[0]);
     }
+
+    zmq_close (subscriber);
+    zmq_ctx_destroy (context);
+    
     return 0;
 }
